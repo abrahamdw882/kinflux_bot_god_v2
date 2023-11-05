218f4f13839b35c9d51523552934e23ddf5f5637
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-11-05 19:29:31 -0300

    Add files via upload

---
+++ plugins/tools-pokedex.js
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'Please provide a Pokemon name to search for.';
+
+  const url = `https://some-random-api.com/pokemon/pokedex?pokemon=${encodeURIComponent(text)}`;
+
+  const response = await fetch(url);
+  const json = await response.json();
+
+  if (!response.ok) {
+    throw `An error occurred: ${json.error}`;
+  }
+
+  const message = `
+*Name:* ${json.name}
+*ID:* ${json.id}
+*Type:* ${json.type}
+*Abilities:* ${json.abilities}
+*Height:* ${json.height}
+*Weight:* ${json.weight}
+*Description:* ${json.description}
+╭───•🎁🎁•───╮
+GENERATED WITH LOVE BY AB'S CHRISTAIN BOT
+╰───•🎁🎁•───╯
+`;
+
+  conn.sendMessage(m.chat, { text: message }, 'extendedTextMessage', { quoted: m });
+};
+
+handler.help = ['pokedex <pokemon>'];
+handler.tags = ['pokemon'];
+handler.command = /^pokedex/i;
+
+export default handler;
