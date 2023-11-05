7a5aa36d77fd379455fa679575b035371f64115c
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-11-05 19:20:00 -0300

    Add files via upload

---
+++ plugins/gp-tagall.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+    m.reply(`▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ kinflux ┃ ᴮᴼᵀ ✪──', null, {
+        mentions: users
+    })
+}
+
+handler.help = ['tagall']
+handler.tags = ['group']
+handler.command = ['tagall']
+handler.admin = true
+handler.owner = true
+handler.group = true
+
+export default handler
