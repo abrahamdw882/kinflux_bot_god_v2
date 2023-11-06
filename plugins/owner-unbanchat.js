e70c5c3bfdbdc1eab754286d41325f5d5d65b8cf
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-11-06 13:21:53 -0300

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner} ) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('✅ AB'S CHRISTAIN BOT is activated in this group')   
+    m.reply('✅ AB CHRISTAIN BOT is activated in this group')   
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
