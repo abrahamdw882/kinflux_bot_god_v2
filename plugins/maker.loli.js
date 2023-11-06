04a10ef423605fcc456e49b7d0088bad0be1f040
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-11-06 13:41:04 -0300

    Update maker.loli.js

--- plugins/maker.loli.js
+++ plugins/maker.loli.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/lolice', {
+    conn.sendFile(m.chat, global.API('https://bible-api.com/romans%2012:1-2,5-7,9,13:1-9&10', '/canvas/misc/lolice', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
     }), 'error.png', '*🚔🚨 𝐋𝐎𝐋𝐈𝐂𝐎𝐍𝐒 like you only belong in jail 🚨🚔*', m)
     }
