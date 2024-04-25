d84cbf49f011d87ca1b5e1ed702b35846e8e2266
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-04-25 19:19:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('kinflux - bot', {
+say('Abraham - bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @kinfluxdigal._`, {
+say(`'${name}' By @Abraham._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
